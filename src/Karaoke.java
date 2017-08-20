e7519507ac686c8dd46f71528ef9e4ac294a0fa5
Viktor Khon <betepokus@yahoo.com>
2017-08-20 01:59:02 -0400

    Save and retrieve file with BookSong

--- src/Karaoke.java
+++ src/Karaoke.java
@@ -5,7 +5,11 @@ import com.viktorkhon.model.SongBook;
 public class Karaoke {
 
     public static void main(String[] args) {
-        KaraokeMachine karaoke = new KaraokeMachine(new SongBook());
+        SongBook songBook = new SongBook();
+        songBook.importFrom("songs.txt");
+        KaraokeMachine karaoke = new KaraokeMachine(songBook);
         karaoke.run();
+        System.out.println("Saving book...");
+        songBook.exportTo("songs.txt");
     }
 }
--- src/com/viktorkhon/model/SongBook.java
+++ src/com/viktorkhon/model/SongBook.java
@@ -1,5 +1,6 @@
 package com.viktorkhon.model;
 
+import java.io.*;
 import java.util.*;
 
 /**
@@ -10,6 +11,45 @@ public class SongBook {
 
     public SongBook() {
         mSongs = new ArrayList<Song>();
+    }
+
+    // Method to save each song to a file
+    public void exportTo(String fileName) {
+        try (
+                // FileOutputStream - A file output stream is an output stream for writing data to a File or to a FileDescriptor
+                FileOutputStream fos = new FileOutputStream(fileName);
+                // PrintWriter - Prints formatted representations of objects to a text-output stream
+                PrintWriter writer = new PrintWriter(fos);
+                ) {
+            for (Song song : mSongs) {
+                writer.printf("%s|%s|%s%n",
+                        song.getmArtist(),
+                        song.getmTitle(),
+                        song.getmVideoUrl());
+            }
+        }  catch (IOException e) {
+            System.out.printf("problem saving %s %n", fileName);
+            e.printStackTrace();
+        }
+    }
+
+    public void importFrom(String fileName) {
+        try (
+                FileInputStream fis = new FileInputStream(fileName);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+                ) {
+            String line;
+            // Note the extra slash before 'line' to encapsulate the expression and then use it to get a boolean
+            while ((line = reader.readLine()) != null) {
+                // Split the string into an array of 3 Strings by separating the "\"
+                String [] args = line.split("\\|");
+                // Create a new Song object by getting an appropriate value from each array index
+                addSong(new Song(args[0], args[1], args[2]));
+            }
+        }  catch (IOException e) {
+            System.out.printf("problem loading %s %n", fileName);
+            e.printStackTrace();
+        }
     }
 
     public void addSong(Song song) {
@@ -23,7 +63,7 @@ public class SongBook {
     //FIX ME - This should be cached
     // Create a Map list of Artist - Song
     private Map<String, List<Song>> byArtist() {
-        Map<String, List<Song>> byArtist = new HashMap<>();
+        Map<String, List<Song>> byArtist = new TreeMap<>();
         // for each of the songs that is currently in the list
         for (Song song : mSongs) {
             // See if the song has already been defined, and if it has get a List back
@@ -49,8 +89,17 @@ public class SongBook {
     }
 
     public List<Song> getSongsForArtist(String artistName) {
-        return byArtist().get(artistName);
+        List<Song> songs = byArtist().get(artistName);
+        // Use Anonymous Class to sort the List
+        songs.sort(new Comparator<Song>() {
+            @Override
+            public int compare(Song song1, Song song2) {
+                if (song1.equals(song2)) {
+                    return 0;
+                }
+                return song1.getmTitle().compareTo(song2.getmTitle());
+            }
+        });
+        return songs;
     }
-
-
 }
